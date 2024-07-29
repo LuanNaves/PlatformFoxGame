c3b69a2a65b7b5e77a43ac8e471a4940aa8f2a60
Luan Felipe <luandhx@gmail.com>
2024-07-29 17:09:49 -0300

    First commit
    Background, player and basic menu added. Working on the enemy

---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump and apply jump animation
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animation_player.play("jump")
+		
+	# Get the input direction a
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	if direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Aplly movimant and animations
+	if direction:
+		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animation_player.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animation_player.play("idle")
+	if velocity.y > 0:
+		animation_player.play("fall")
+		
+	move_and_slide()
